e8bcebb541fb701a8ac1812ef64efe1bc0434450
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-30 09:35:39 +0700

    memperbarui file code PengelolaHalaman

--- app/src/main/java/com/example/pertemuan12/ui/theme/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan12/ui/theme/navigation/PengelolaHalaman.kt
@@ -1,2 +1,38 @@
 package com.example.pertemuan12.ui.theme.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan12.ui.theme.view.DestinasiEntry
+import com.example.pertemuan12.ui.theme.view.DestinasiHome
+import com.example.pertemuan12.ui.theme.view.EntryMhsScreen
+import com.example.pertemuan12.ui.theme.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
