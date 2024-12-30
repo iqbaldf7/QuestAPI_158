436e668ec582d4aab4f412266d26672f2bb75827
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-30 09:57:34 +0700

    memperbaiki beberapa eror di MahasiswaRepository

--- app/src/main/java/com/example/pertemuan12/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/pertemuan12/repository/MahasiswaRepository.kt
@@ -3,49 +3,53 @@ import com.example.pertemuan12.model.Mahasiswa
 import com.example.pertemuan12.service.MahasiswaService
 import java.io.IOException
 
-interface MahasiswaRepository {
+interface MahasiswaRepository{
     suspend fun getMahasiswa(): List<Mahasiswa>
 
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 
     suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
 
-    suspend fun  deleteMahasiswa(nim: String)
+    suspend fun deleteMahasiswa(nim: String)
 
-    suspend fun  getMahasiswabyNim(nim: String): Mahasiswa
+    suspend fun getMahasiswabyNim(nim: String): Mahasiswa
 }
 
-class MahasiswaKontakRepository(
-    private val kontakApiService: MahasiswaService
-):MahasiswaRepository{
-    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
-        kontakApiService.insertMahasiswa(mahasiswa)
+class NetworkMahasiswaRepository(
 
+    private val mahasiswaApiService: MahasiswaService
+
+) : MahasiswaRepository{
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
     }
 
     override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
-        kontakApiService.updateMahasiswa(nim, mahasiswa)
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
     }
 
     override suspend fun deleteMahasiswa(nim: String) {
-        try{
-            val response = kontakApiService.deleteMahasiswa(nim)
-            if(!response.isSuccessful){
-                throw IOException("Failed to delete kontak. HTTP status code: " + "${response.code()}")
-            }else{
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete kontak. HTTP Status code : ")
+                        "${response.code()}"
+            } else {
                 response.message()
                 println(response.message())
             }
-        }catch (e: Exception){
+        }
+        catch (e: Exception){
             throw e
         }
     }
 
-    override suspend fun getMahasiswa(): List<Mahasiswa> =
-        kontakApiService.getAllMahasiswa()
+    override suspend fun getMahasiswa(): List<Mahasiswa> {
+        return mahasiswaApiService.getAllMahasiswa()
+    }
 
     override suspend fun getMahasiswabyNim(nim: String): Mahasiswa {
-        return kontakApiService.getMahasiswabyNim(nim)
+        return mahasiswaApiService.getMahasiswabyNim(nim)
     }
 
 }
