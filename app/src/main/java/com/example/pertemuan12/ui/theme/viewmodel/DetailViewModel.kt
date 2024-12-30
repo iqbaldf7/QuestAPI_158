03f5fb1c2d6c1b3e6863c146743ee53eeb99545f
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-12-30 10:39:00 +0700

    menambah file DetailViewModel ke dalam package viewmodel

---
+++ app/src/main/java/com/example/pertemuan12/ui/theme/viewmodel/DetailViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.pertemuan12.ui.theme.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+
+class DetailViewModel(private val mhsRepository: MahasiswaRepository) : ViewModel() {
+
+    var uiState by mutableStateOf(DetailUiState())
+        private set
+
+    fun fetchDetailMahasiswa(nim: String) {
+        viewModelScope.launch {
+            uiState = DetailUiState(isLoading = true)
+            try {
+                val mahasiswa = mhsRepository.getMahasiswabyNim(nim)
+                uiState = DetailUiState(detailUiEvent = mahasiswa.toDetailUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                uiState = DetailUiState(isError = true, errorMessage = "Failed to fetch details: ${e.message}")
+            }
+        }
+    }
+}
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
+
+fun Mahasiswa.toDetailUiEvent(): InsertUiEvent {
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        alamat = alamat,
+        jenisKelamin = jenisKelamin,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
